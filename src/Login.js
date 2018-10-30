3f2e49276f549cb2ce93ef5dff429f2d72669b9a
Nazmul Hossain <aknazmul@gmail.com>
2018-10-30 11:00:32 +0600

    login page finalize

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 
-import React from 'react'; 
+import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
@@ -7,13 +7,16 @@ import TextField from '@material-ui/core/TextField';
 import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
 // import { lightBlue, purple } from '@material-ui/core/colors';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent'; 
+import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 const theme = createMuiTheme({
     palette: { 
         primary: {
             main: '#027abb',
+        },
+        primaryLight: {
+            main: '#E3F6FF',
         },
         secondary: {
             main: '#3C4042',
@@ -23,6 +26,7 @@ const theme = createMuiTheme({
 
 const styles = {
     authWrapper: {
+        background: '#E3F6FF',
         position: 'absolute',
         width: '100%',
         height: '100%',
@@ -30,6 +34,22 @@ const styles = {
     row: {
         height: '100%',
     },
+    blockWidth: {
+        width: '100%',
+    },
+    mb2: {
+        margin: '0 0 15px 0',
+    },
+    pa0: {
+        padding: '0!important',
+    },
+    authTop: {
+        padding: '30px 50px 15px',
+    },
+    authBottom: {
+        padding: '25px 50px 25px 50px',
+        background: '#E3F6FF',
+    }
 };
 
 function Login(props) { 
@@ -43,49 +63,76 @@ function Login(props) {
                     justify="center"
                     alignItems="center" 
                     >
-                    <Grid item xs={10}> 
+                    <Grid item sm={12} md={3}> 
                         <Card>
-                            <CardContent>
-                            <Grid container>
-                                <Grid item xs={6}>
-                                <Typography paragraph={true}>
-                                    Met je login gegevens krijg je toegang tot het IT-Button.com dashboard. Dit is jouw persoonlijke omgeving waar je alle informatie over je projecten kunt raadplegen, wijzigen en je software kunt testen. Heb je nog geen login? Klik dan hier!
-                                </Typography>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <TextField 
-                                        label="E-mail"
-                                        placeholder="Enter your email address"
-                                        className="textField"
-                                        margin="normal"
-                                        color="primary"
-                                        // onChange={(event, newValue) => this.setState({ username: newValue })}
-                                    /> 
-                                    <br/>
-                                    <TextField
-                                        type="password" 
-                                        label="Password"
-                                        placeholder="Enter your Password"
-                                        className="textField"
-                                        color="primary"
-                                        // onChange={(event, newValue) => this.setState({ password: newValue })}
-                                    />
-                                    <br/>                                
-                                    <Button variant="contained" color = "primary">
-                                        Login
-                                    </Button> 
-                                    <Button variant="contained" color = "secondary">
-                                        secondary
-                                    </Button> 
-                                    {/* <Button variant="contained" color = "primary" onClick={(event) => {
-                                        if(this.handleValidation())
-                                            this.handleClick(event)
-                                        else 
-                                            alert("Enter a username")}}>
-                                        Login
-                                    </Button>  */}
-                                </Grid>
-                            </Grid> 
+                            <CardContent className={classes.pa0}> 
+                                <div className={classes.authTop}> 
+                                    <Typography className={classes.mb2} variant="h3">
+                                    Login to your dashboard.
+                                    </Typography> 
+
+                                    <Typography>
+                                    With your log-in details, you can access the IT-Button.com dashboard. This is your personal environment where you can consult and change all of your project information and also test your software. Still don't have a log-in? Click here!
+                                    </Typography>
+ 
+                                        <div className={classes.mb2}>
+                                            <TextField 
+                                                label="E-mail"
+                                                placeholder="Enter your email address"
+                                                className={classes.blockWidth}
+                                                margin="normal"
+                                                color="primary"
+                                                // onChange={(event, newValue) => this.setState({ username: newValue })}
+                                            />    
+                                        </div>
+
+                                        <div className={classes.mb2}>
+                                            <TextField
+                                                type="password" 
+                                                label="Password"
+                                                placeholder="Enter your Password"
+                                                className={classes.blockWidth}
+                                                color="primary"
+                                                // onChange={(event, newValue) => this.setState({ password: newValue })}
+                                            />          
+                                        </div>       
+
+                                        <div className={classes.mb2}>              
+                                            <Button variant="contained" className={classes.blockWidth} color = "primary">
+                                                Submit
+                                            </Button>
+                                        </div>
+                                        </div>
+                                        <Grid className={classes.mb2} container
+                                            direction="column"
+                                            justify="center"
+                                            alignItems="center">  
+                                            <Button color = "primary">
+                                                Forgot your password?
+                                            </Button>
+                                        </Grid>
+
+                                        <div className={classes.authBottom}>
+                                            <Grid 
+                                                container
+                                                direction="row"
+                                                justify="center"
+                                                alignItems="center">
+                                                <span className={classes.mb2}> Still no log-in details?</span>
+                                                <Button variant="outlined" color = "secondary">
+                                                    Make your account here!
+                                                </Button>
+                                            </Grid>
+                                        </div>
+
+                                        {/* <Button variant="contained" color = "primary" onClick={(event) => {
+                                            if(this.handleValidation())
+                                                this.handleClick(event)
+                                            else 
+                                                alert("Enter a username")}}>
+                                            Login
+                                        </Button>  */}  
+                                
                             </CardContent> 
                         </Card>
                     </Grid>
